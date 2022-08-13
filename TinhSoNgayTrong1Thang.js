51e0bf7d34216d935330b7d965e00d04834fb6bf
Admin <trongtin100@gmail.com>
2022-08-13 22:25:45 +0700

    feature05-TinhSoNgayTrong1Thang

---
+++ TinhSoNgayTrong1Thang.js
@@ -0,0 +1,39 @@
+let songay = prompt ("soNgayTrong1thang" + "");
+switch (songay) {
+    case "1":
+        alert("Thang1co31ngay");
+        break;
+    case "3":
+        alert("Thang3co31ngay");
+        break;
+    case "4":
+        alert("Thang4co31ngay");
+        break;
+    case "5":
+        alert("Thang5co31ngay");
+        break;
+    case "6":
+        alert("Thang6co31ngay");
+        break;
+    case "7":
+        alert("Thang7co31ngay");
+        break;
+    case "8":
+        alert("Thang8co31ngay");
+        break;
+    case "9":
+        alert("Thang9co31ngay");
+        break;
+    case "10":
+        alert("Thang10co31ngay");
+        break;
+    case "11":
+        alert("Thang11co31ngay");
+        break;
+    case "12":
+        alert("Thang12co31ngay");
+        break;
+    case "2":
+        alert("Thang2co28-29ngay");
+        break;
+}
